d27b365f265074f3c227e4110593b16f604a0537
Eddo Careera Iriyanto Putra <eddocareera.i.p@gmail.com>
2022-10-12 20:29:21 +0700

    Initial Commit

---
+++ MacroComponentExercise/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MacroComponentExercise
+//
+//  Created by Eddo Careera Iriyanto Putra on 12/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MacroComponentExercise/MacroComponentExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacroComponentExerciseApp.swift
+//  MacroComponentExercise
+//
+//  Created by Eddo Careera Iriyanto Putra on 12/10/22.
+//
+
+import SwiftUI
+
+@main
+struct MacroComponentExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
